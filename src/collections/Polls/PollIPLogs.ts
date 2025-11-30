b54924a8ebc0ee00f86eb50e66fcf2502765694e
metrowave-digital <krclemons97@gmail.com>
2025-11-30 18:23:14 +0100

    delete poll ip logs

--- src/collections/Polls/PollIPLogs.ts
+++
@@ -1,9 +0,0 @@
-import type { CollectionConfig } from 'payload'
-
-export const PollIPLogs: CollectionConfig = {
-  slug: 'poll-ip-logs',
-  fields: [
-    { name: 'pollId', type: 'number', required: true },
-    { name: 'ip', type: 'text', required: true },
-  ],
-}
