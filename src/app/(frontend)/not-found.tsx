df5dca3320e72cff4d5f98029d7bb6f576480319
metrowave-digital <krclemons97@gmail.com>
2025-11-30 01:25:32 +0100

    pages update

--- src/app/(frontend)/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>404 – Page Not Found</h1>
-      <p>The page you’re looking for does not exist.</p>
-    </div>
-  )
-}
